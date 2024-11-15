ff4b33e496fbdda878434ff56873ebcd3f11585e
Na Cai <caina89@gmail.com>
2024-11-15 16:26:26 +0100

    Update 1000G_snps.R

--- Preprocessing/1000G_snps.R
+++ Preprocessing/1000G_snps.R
@@ -23,3 +23,14 @@ for (chr in chromosomes){
 	write.table(gtexchr[,c(1:6)],paste0(wdir,"/genotypes/chr",chr,".maf5hwe6missing10.kgp.extract"),quote=F, row.names=F, col.names=F, sep="\t")
 	write.table(gtexchr[,c(1,8,3,4,5,6)],paste0(wdir,"/genotypes/chr",chr,".maf5hwe6missing10.kgp.rsids"),quote=F, row.names=F, col.names=F, sep="\t")
 }
+
+## find SNPs with discordant alleles 
+wdir="/GTExV8/genotypes"
+kgpdir="/1000G/GRCh38"
+gtex=read.table(paste0(wdir,"/allchr.maf5hwe6missing10.kgp.ld2.bim"),as.is=T)
+kgp=read.table(paste0(kgpdir,"/allchr.snps.maf001.hwe.geno9.biallelic.gtex.ld2.bim"),as.is=T)
+r=which(gtex$V5!=kgp$V5|gtex$V6!=kgp$V6)
+k=which(gtex$V5==kgp$V6&gtex$V6==kgp$V5)
+r=r[which(!r%in%k)]
+remove=gtex[r,]
+write.table(remove, paste0(wdir,"/discordant.exclude"),quote=F, row.names=F, col.names=F, sep="\t")
